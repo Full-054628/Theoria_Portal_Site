52beebce785ca97c8727fbc466fe0fbb86335e0d
Full-054628 <full054628uni@gmail.com>
2024-05-12 19:47:33 +0900

    #1_土台となるTODOアプリケーションを実装。

---
+++ src/main/java/com/example/TheoriaPortalSite/TheoriaPortalSiteApplication.java
@@ -0,0 +1,13 @@
+package com.example.TheoriaPortalSite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheoriaPortalSiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheoriaPortalSiteApplication.class, args);
+	}
+
+}
